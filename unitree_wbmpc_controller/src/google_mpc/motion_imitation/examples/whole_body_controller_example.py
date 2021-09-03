d03d18a95bacae9e5db222771e8553729a4c6119
Ramil <r.khusainov@innopolis.ru>
2021-09-03 16:46:55 +0300

    foot friction changed

--- unitree_wbmpc_controller/src/google_mpc/motion_imitation/examples/whole_body_controller_example.py
+++ unitree_wbmpc_controller/src/google_mpc/motion_imitation/examples/whole_body_controller_example.py
@@ -86,12 +86,12 @@ _INIT_LEG_STATE = (
 
 def _generate_example_linear_angular_speed(t):
   """Creates an example speed profile based on time for demo purpose."""
-  # vx = 0.6
-  # vy = 0.2
-  # wz = 0.8
-  vx = 0.0
-  vy = 0.0
-  wz = 0.0
+  vx = 0.6
+  vy = 0.2
+  wz = 0.8
+  # vx = 0.0
+  # vy = 0.0
+  # wz = 0.0
 
   time_points = (0, 5, 10, 15, 20, 25, 30)
   speed_points = ((0, 0, 0, 0), (0, 0, 0, wz), (vx, 0, 0, 0), (0, 0, 0, -wz),
